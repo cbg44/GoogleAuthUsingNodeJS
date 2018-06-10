84fc60d4971cc54fe7912ce2feac4d178e6cf434
elaya38 <elaya.gabay@gmail.com>
2018-06-10 15:24:37 +0300

    changing schema

--- models/userDAO.js
+++ models/userDAO.js
@@ -5,17 +5,17 @@ Tasks = require('../models/tasksModel');
 
 module.exports = class userDAO {
     static mostPointsThisWeek() {
-        return User.find(req.user).sort({scores: 'desc'})
+        return User.find().sort({scores: 'desc'})
                    .catch(() => error("bla"));
     }
 
   static mostTasksDoneSoFar() {
-    return User.find().sort({scores: 'desc'})
+    return User.find({}, 'name id taskcounter').sort({taskcounter: 'desc'})
       .catch(() => error("bla"));
   }
 
   static TasksPerDay() {
-    return User.find({}, 'name scores')
+    return User.find({}, 'name id dailytaskscounter').sort({dailytaskscounter: 'desc'})
       .catch(() => error("bla"));
   }
 
